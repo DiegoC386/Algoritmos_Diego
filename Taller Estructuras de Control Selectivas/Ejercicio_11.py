9f56c0be8ed30ec191d97be912f55a0d53db13c8
Diego Calderon <>
2021-04-23 09:29:48 +0000

    Taller 4

---
+++ Taller Estructuras de Control Selectivas/Ejercicio_11.py
@@ -0,0 +1,23 @@
+"""
+Desarrolle un algoritmo, que dado como dato una temperatura en grados
+Fahrenheit,determine el deporte que es apropiado practicar a esa
+temperatura, teniendo en cuenta la siguiente tabla:
+
+Entradas
+temperatura---float--t
+Salida
+tipo_de_natacion-->str--tipo
+"""
+t=float(input("Digite temperatura: "))
+if(t>85):
+  print("natación")
+elif(t>=71 and t<=85):
+  print("tenis")
+elif(t>=33 and t<=70):
+  print("Golf")
+elif(t>=11 and t<=32):
+  print("Esquí")
+elif(t<=10):
+  print("Marcha")
+else:
+  print("No se identifico deporte")
