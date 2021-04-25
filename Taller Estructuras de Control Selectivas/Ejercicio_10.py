9a503064c2fa171691708d6f1e46f1bc309599ab
Diego Calderon <>
2021-04-25 12:26:01 +0000

    Taller 4

--- Taller Estructuras de Control Selectivas/Ejercicio_10.py
+++ Taller Estructuras de Control Selectivas/Ejercicio_10.py
@@ -3,3 +3,28 @@ Construya un programa en Python que, dados como datos la categoría y el sueldo 
 del trabajador, calcule el aumento correspondiente teniendo en cuenta la siguiente tabla:
 Como salida, mostrar la categoría del trabajador y su nuevo sueldo bruto.
 """
+SueldoBruto=int(input())
+if(SueldoBruto<=900000 ):
+	NuevoSueldo= (SueldoBruto*0.6)+SueldoBruto
+	print("Su Nuevo Sueldo es: $ {:.0f}".format(NuevoSueldo))
+	print("Su Categoria es la 5")
+elif(SueldoBruto>900000 and SueldoBruto<=2000000):
+	NuevoSueldo= (SueldoBruto*0.4)+SueldoBruto
+	print("Su Nuevo Sueldo es: $ {:.0f}".format(NuevoSueldo))
+	print("Su Categoria es la 4")
+elif(SueldoBruto>2000000 and SueldoBruto<=3600000):
+	NuevoSueldo= (SueldoBruto*0.2)+SueldoBruto
+	print("Su Nuevo Sueldo es: $ {:.0f}".format(NuevoSueldo))
+	print("Su Categoria es la 3")
+elif(SueldoBruto>3600000 and SueldoBruto<=4300000):
+	NuevoSueldo= (SueldoBruto*0.15)+SueldoBruto
+	print("Su Nuevo Sueldo es: $ {:.0f}".format(NuevoSueldo))
+	print("Su Categoria es la 2")
+elif(SueldoBruto>4300000 and SueldoBruto<=5000000):
+	NuevoSueldo= (SueldoBruto*0.10)+SueldoBruto
+	print("Su Nuevo Sueldo es: $ {:.0f}".format(NuevoSueldo))
+	print("Su Categoria es la 1")
+else:
+	print("No se puede calcular")
+
+	
--- Taller Estructuras de Control Selectivas/Ejercicio_9.py
+++ Taller Estructuras de Control Selectivas/Ejercicio_9.py
@@ -12,3 +12,43 @@ e.	Si el monto es mayor a $15000, hay un 25% de descuento.
 Calcule y muestre el nombre del cliente, el monto de la compra, monto a pagar
 y descuento recibido.
 """
+Nombre=input()
+Compra=int(input())
+if(Compra>0 and Compra<50000):
+	print("Nombre del cliente: "+ str(Nombre))
+	print("Monto de la compra: $ {:.0f}".format(Compra))
+	print("No hay descuento")
+	print("Monto a Pagar: $ {:.0f}".format(Compra))
+elif(Compra>=50000 and Compra<100000):
+	Desc=(Compra*0.05)
+	MontoPagar=(Compra-Desc)
+	print("Nombre del cliente: "+ str(Nombre))
+	print("Monto de la compra: $ {:.0f}".format(Compra))
+	print("Descuento Recibido: $ {:.0f}".format(Desc))
+	print("Monto a Pagar: $ {:.0f}".format(MontoPagar))
+elif(Compra>=100000 and Compra<700000):
+	Desc=(Compra*0.11)
+	MontoPagar=(Compra-Desc)
+	print("Nombre del cliente: "+ str(Nombre))
+	print("Monto de la compra: $ {:.0f}".format(Compra))
+	print("Descuento Recibido: $ {:.0f}".format(Desc))
+	print("Monto a Pagar: $ {:.0f}".format(MontoPagar))
+elif(Compra>=700000 and Compra<1500000):
+	Desc=(Compra*0.18)
+	MontoPagar=(Compra-Desc)
+	print("Nombre del cliente: "+ str(Nombre))
+	print("Monto de la compra: $ {:.0f}".format(Compra))
+	print("Descuento Recibido: $ {:.0f}".format(Desc))
+	print("Monto a Pagar: $ {:.0f}".format(MontoPagar))
+elif(Compra>=1500000):
+	Desc=(Compra*0.25)
+	MontoPagar=(Compra-Desc)
+	print("Nombre del cliente: "+ str(Nombre))
+	print("Monto de la compra: $ {:.0f}".format(Compra))
+	print("Descuento Recibido: $ {:.0f}".format(Desc))
+	print("Monto a Pagar: $ {:.0f}".format(MontoPagar))	
+else:
+	print("ERROR")
+
+
+	
