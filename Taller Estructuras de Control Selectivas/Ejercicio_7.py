c867e93b2c9f0a88fcb97914c69088d4e2400133
Diego Calderon <>
2021-04-26 23:47:36 +0000

    Taller 4

--- Taller Estructuras de Control Selectivas/Ejercicio_7.py
+++ Taller Estructuras de Control Selectivas/Ejercicio_7.py
@@ -7,7 +7,7 @@ if(Km>0 and Km<300):
 	Paga=50000
 	print("Total a pagar es: $ {:.0f}".format(Paga))
 elif(Km>300 and Km<1000):
-	Total=(((1000-Km)*30000)+70000)
+	Total=(((Km-300)*30000)+70000)
 	print("Total a pagar es: $ {:.0f}".format(Total))
 elif(Km>1000):
 	Total1=(((Km-1000)*9000)+150000)
