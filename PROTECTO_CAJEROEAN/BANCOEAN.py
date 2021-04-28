e136c6b2655434e7972e5c601f9d8a7c561caef3
Diego Calderón <>
2021-04-28 03:40:48 +0000

    Proyecto

---
+++ PROTECTO_CAJEROEAN/BANCOEAN.py
@@ -0,0 +1,24 @@
+"""
+2 usuario, contraseñas
+2 cuentas
+Cajero=2000000
+Opcion1=BancoEan
+Menu
+RetiroLimite
+Transferencia
+Cunsulta
+
+VARIABLES
+
+
+
+Opscion2=RestauranteEan
+Menu
+
+
+Opcion3=Parqueadero
+Menu
+
+
+
+"""
