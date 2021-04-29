aaa5319a946d39e4c3375a380dc28ce7adfd9a91
Diego Calderón <>
2021-04-29 01:10:31 +0000

    2

---
+++ PROTECTO_CAJEROEAN/Ejemplo.py
@@ -0,0 +1,10 @@
+usuario=input("Digite Usuario: ")
+contraseña=input("Digite Contraseña: ")
+lista=["brayan","123"]
+while True:#indeterminado
+	usuario=input("Digite Usuario: ")
+	contraseña=input("Digite Contraseña: ")
+if(usuario==lista[0]):
+	if(contraseña==lista[1]):
+		print("Bienvenido")
+		break:
